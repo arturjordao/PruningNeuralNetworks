4cf9fe9a268e8a1f39ac9faf014d69077aade4f3
arturjordao <arturjordao@users.noreply.github.com>
2020-04-25 14:55:50 -0300

    Update main.py
    Pruning VGG-based with fully connected layers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.metrics.classification import accuracy_score
-from sklearn.model_selection import train_test_split
+import argparse
 import keras
 from keras.layers.pooling import GlobalMaxPooling2D, GlobalAveragePooling2D, MaxPooling2D, AveragePooling2D
 from keras.layers import Dense, Dropout, Conv2D, Flatten, Activation, BatchNormalization, Add
@@ -8,6 +8,19 @@ from keras.layers import Input
 from keras.models import Model
 
 from VIPPruning import VIPPruning
+
+def layers_to_prune(model):
+    # Convert index into Conv2D index (required by pruning methods)
+    idx_Conv2D = 0
+    output = []
+    for i in range(0, len(model.layers)):
+        if isinstance(model.get_layer(index=i), Conv2D):
+            output.append(idx_Conv2D)
+            idx_Conv2D = idx_Conv2D + 1
+
+    #Exception for VGG-Based architectures
+    output.pop(-1)
+    return output
 
 def rebuild_net(model=None, layer_filters=[]):
     n_discarded_filters = 0
@@ -19,11 +32,15 @@ def rebuild_net(model=None, layer_filters=[]):
 
     H = Input(inp)
     inp = H
+    idxs = []
     idx_previous = []
 
-    for i in range(1, len(model.layers)):
+    for i in range(0, len(model.layers)+1):#There is bug; it is necessary use model.layers+1
 
-        layer = model.get_layer(index=i)
+        try:
+            layer = model.get_layer(index=i)
+        except:
+            break
         config = layer.get_config()
 
         if isinstance(layer, MaxPooling2D):
@@ -49,7 +66,10 @@ def rebuild_net(model=None, layer_filters=[]):
             n_filters = weights[0].shape[3]
             total_filters = total_filters + n_filters
 
-            idxs = [item for item in layer_filters if item[0] == i][0][1]
+            #idxs = [item for item in layer_filters if item[0] == i][0][1]
+            idxs = [item for item in layer_filters if item[0] == i]
+            if len(idxs)!=0:
+                idxs = idxs[0][1]
 
             weights[0] = np.delete(weights[0], idxs, axis=3)
             weights[1] = np.delete(weights[1], idxs)
@@ -78,91 +98,28 @@ def rebuild_net(model=None, layer_filters=[]):
                        weights=weights
                        )(H)
 
+        elif isinstance(layer, Flatten):
+            H = Flatten()(H)
+
+        elif isinstance(layer, Dense):
+            weights = layer.get_weights()
+            weights[0] = np.delete(weights[0], idx_previous, axis=0)
+            H = Dense(units=config['units'],
+                      activation=config['activation'],
+                      activity_regularizer=config['activity_regularizer'],
+                      bias_constraint=config['bias_constraint'],
+                      bias_regularizer=config['bias_regularizer'],
+                      kernel_constraint=config['kernel_constraint'],
+                      kernel_regularizer=config['kernel_regularizer'],
+                      name=config['name'],
+                      trainable=config['trainable'],
+                      use_bias=config['use_bias'],
+                      weights=weights)(H)
+            idxs = []#After the first Dense Layer the methods stop prunining
+
         idx_previous = idxs
-    print('Percentage of discarded filters {}'.format(n_discarded_filters / float(total_filters)))
+    #print('Percentage of discarded filters {}'.format(n_discarded_filters / float(total_filters)))
     return Model(inp, H)
-
-def generate_conv_model(model):
-
-    model = model.get_layer(index=1)
-
-    inp = (model.inputs[0].shape.dims[1].value,
-           model.inputs[0].shape.dims[2].value,
-           model.inputs[0].shape.dims[3].value)
-
-    H = Input(inp)
-    inp = H
-
-    for layer_idx in range(1, len(model.layers)):
-
-        layer = model.get_layer(index=layer_idx)
-        config = layer.get_config()
-
-        if isinstance(layer, MaxPooling2D):
-            H = MaxPooling2D.from_config(config)(H)
-
-        if isinstance(layer, Dropout):
-            H = Dropout.from_config(config)(H)
-
-        if isinstance(layer, Activation):
-            H = Activation.from_config(config)(H)
-
-        if isinstance(layer, BatchNormalization):
-            weights = layer.get_weights()
-            H = BatchNormalization(weights=weights)(H)
-
-        elif isinstance(layer, Conv2D):
-            weights = layer.get_weights()
-
-            config['filters'] = weights[1].shape[0]
-            H = Conv2D(activation=config['activation'],
-                       activity_regularizer=config['activity_regularizer'],
-                       bias_constraint=config['bias_constraint'],
-                       bias_regularizer=config['bias_regularizer'],
-                       data_format=config['data_format'],
-                       dilation_rate=config['dilation_rate'],
-                       filters=config['filters'],
-                       kernel_constraint=config['kernel_constraint'],
-                       kernel_regularizer=config['kernel_regularizer'],
-                       kernel_size=config['kernel_size'],
-                       name=config['name'],
-                       padding=config['padding'],
-                       strides=config['strides'],
-                       trainable=config['trainable'],
-                       use_bias=config['use_bias'],
-                       weights=weights
-                       )(H)
-
-    return Model(inp, H)
-
-def frozen_conv2D(model, trainable=False):
-    #Frozen the convolutional layers
-    try:
-        model.get_layer(index=1).trainable = trainable
-        for layer in model.get_layer(index=1).layers:
-            layer.trainable = trainable
-    except:
-        model.trainable = trainable
-        for layer in model.layers:
-            layer.trainable = trainable
-
-    return model
-
-def insert_fully(cnn_model, input_shape=(32, 32, 3), num_classes=10):
-
-    inp = Input(input_shape)
-    H = Model(cnn_model.input, cnn_model.layers[-1].output)
-    H = H(inp)
-
-    H = Flatten()(H)
-    H = Dense(512)(H)
-    H = Activation('relu')(H)
-    H = Dropout(0.5)(H)
-    H = Dense(num_classes)(H)
-    H = Activation('softmax')(H)
-
-    model = keras.models.Model(inp, H)
-    return model
 
 def count_filters(model):
     n_filters = 0
@@ -175,8 +132,73 @@ def count_filters(model):
 
     return n_filters
 
+def compute_flops(model):
+    import keras
+    total_flops =0
+    flops_per_layer = []
+
+    try:
+        layer = model.get_layer(index=1).layers #Just for discover the model type
+        for layer_idx in range(1, len(model.get_layer(index=1).layers)):
+            layer = model.get_layer(index=1).get_layer(index=layer_idx)
+            if isinstance(layer, keras.layers.Conv2D) is True:
+                _, output_map_H, output_map_W, current_layer_depth = layer.output_shape
+
+                _, _, _, previous_layer_depth = layer.input_shape
+                kernel_H, kernel_W = layer.kernel_size
+
+                flops = output_map_H * output_map_W * previous_layer_depth * current_layer_depth * kernel_H * kernel_W
+                total_flops += flops
+                flops_per_layer.append(flops)
+
+        for layer_idx in range(1, len(model.layers)):
+            layer = model.get_layer(index=layer_idx)
+            if isinstance(layer, keras.layers.Dense) is True:
+                _, current_layer_depth = layer.output_shape
+
+                _, previous_layer_depth = layer.input_shape
+
+                flops = current_layer_depth * previous_layer_depth
+                total_flops += flops
+                flops_per_layer.append(flops)
+    except:
+        for layer_idx in range(1, len(model.layers)):
+            layer = model.get_layer(index=layer_idx)
+            if isinstance(layer, keras.layers.Conv2D) is True:
+                _, output_map_H, output_map_W, current_layer_depth = layer.output_shape
+
+                _, _, _, previous_layer_depth = layer.input_shape
+                kernel_H, kernel_W = layer.kernel_size
+
+                flops = output_map_H * output_map_W * previous_layer_depth * current_layer_depth * kernel_H * kernel_W
+                total_flops += flops
+                flops_per_layer.append(flops)
+
+            if isinstance(layer, keras.layers.Dense) is True:
+                _, current_layer_depth = layer.output_shape
+
+                _, previous_layer_depth = layer.input_shape
+
+                flops = current_layer_depth * previous_layer_depth
+                total_flops += flops
+                flops_per_layer.append(flops)
+
+    return total_flops, flops_per_layer
+
 if __name__ == '__main__':
     np.random.seed(12227)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--iterations', type=int, default=5)
+    parser.add_argument('--p', type=float, default=0.05)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--n_components', type=int, default=2)
+
+    args = parser.parse_args()
+    iterations = args.iterations
+    p = args.p
+    epochs = args.epochs
+    n_components = args.n_components
 
     (X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
 
@@ -184,16 +206,17 @@ if __name__ == '__main__':
     y_train = keras.utils.to_categorical(y_train, 10)
     y_test = keras.utils.to_categorical(y_test, 10)
 
+    #The architecture we gonna pruning
     input = Input((32, 32, 3))
-    H = Conv2D(32, (3,3), padding='same')(input)
+    H = Conv2D(16, (3,3), padding='same')(input)
     H = Activation('relu')(H)
-    H = Conv2D(32, (3, 3))(H)
+    H = Conv2D(16, (3, 3))(H)
     H = Activation('relu')(H)
     H = MaxPooling2D(pool_size=(2, 2))(H)
 
-    H = Conv2D(64, (3, 3), padding='same')(H)
+    H = Conv2D(32, (3, 3), padding='same')(H)
     H = Activation('relu')(H)
-    H = Conv2D(64, (3, 3))(H)
+    H = Conv2D(32, (3, 3))(H)
     H = Activation('relu')(H)
     H = MaxPooling2D(pool_size=(2, 2))(H)
 
@@ -208,50 +231,35 @@ if __name__ == '__main__':
     cnn_model = keras.models.Model([input], H)
     cnn_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
-    cnn_model.fit(X_train, y_train, epochs=50, batch_size=128, verbose=2)
+    cnn_model.fit(X_train, y_train, epochs=epochs, batch_size=128, verbose=0)
     y_pred = cnn_model.predict(X_test)
     acc = accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
 
     n_params = cnn_model.count_params()
     n_filters = count_filters(cnn_model)
-    print('Original Network. #Parameters [{}] #Filters [{}] Accuracy [{:.4f}]'.format(n_params, n_filters, acc))
+    flops, _ = compute_flops(cnn_model)
+    print('Original Network. #Parameters [{}] #Filters [{}] FLOPs [{}] Accuracy [{:.4f}]'.format(n_params, n_filters, flops, acc))
 
-    iterations = 5
-    p = 0.05
+    layers = layers_to_prune(cnn_model)
 
-    #We need to remove the layers (i.e., fully connected) from flatten layer
-    #to avoid that the rebuild process adds extra layers.
-    for i in range(0, 6):
-        cnn_model.layers.pop()
-
-    cnn_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
     for i in range(0, iterations):
 
-        pruning_method = VIPPruning(n_comp=2, model=cnn_model, representation='max', percentage_discard=p)
-        # pruning_method = VIPPruning(n_comp=2, model=cnn_model,
+        pruning_method = VIPPruning(n_comp=n_components, model=cnn_model, representation='max', percentage_discard=p)
+        # pruning_method = VIPPruning(n_comp=n_components, model=cnn_model,
         #                             representation=MaxPooling2D(pool_size=(2, 2),
         #                                                         name='vip_net'),
         #                             percentage_discard=p)
 
-        idxs = pruning_method.idxs_to_prune(X_train, y_train, [])
+        idxs = pruning_method.idxs_to_prune(X_train, y_train, layers)
         cnn_model = rebuild_net(cnn_model, idxs)
 
-        # First, we fine-tune the Fully Connected Layers only
-        cnn_model = frozen_conv2D(cnn_model, False)
-        model = insert_fully(cnn_model=cnn_model, num_classes=10)
-        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-        model.fit(X_train, y_train, epochs=5, batch_size=128, verbose=2)
+        cnn_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+        cnn_model.fit(X_train, y_train, epochs=epochs, batch_size=128, verbose=0)
 
-        # Finally, we fine-tune the entire network (Fully Connected Layers and Convolutional Layers)
-        model = frozen_conv2D(model, True)
-        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-        model.fit(X_train, y_train, epochs=45, batch_size=128, verbose=2)
-
-        y_pred = model.predict(X_test)
+        y_pred = cnn_model.predict(X_test)
         acc = accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
 
-        n_params  = model.count_params()
-        n_filters = count_filters(model.get_layer(index=1))
-        print('Iteration [{}] #Parameters [{}] #Filters [{}] Accuracy [{:.4f}]'.format(i, n_params, n_filters, acc))
-
-        cnn_model = generate_conv_model(model=model)
+        n_params = cnn_model.count_params()
+        n_filters = count_filters(cnn_model)
+        flops, _ = compute_flops(cnn_model)
+        print('Iteration [{}] #Parameters [{}] #Filters [{}] FLOPs [{}] Accuracy [{:.4f}]'.format(i, n_params, n_filters, flops, acc))
